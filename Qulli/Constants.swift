82ddc031dca5e8cafd34f7bdecb29d835bd56f1f
Sud <sud@Suds-Mac.local>
2018-02-21 11:13:39 +0530

    sud

---
+++ Qulli/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Qulli
+//
+//  Created by Sud on 30/01/18.
+//  Copyright © 2018 Kamal rastogi. All rights reserved.
+//
+
+import Foundation
+struct Constants{
+    //static let BASE_URL = "http://qulli.co/api/"
+    static let BASE_URL = "https://project-qulli-cammy92.c9users.io/api/";
+    static let FORGOT_PASSWORD = BASE_URL+"v1.0/driver/forgot-password"
+    static let LOGIN = BASE_URL+"v1.0/driver/login"
+    static let ADD_IMAGE = BASE_URL+"v1.0/driver/add/image"
+    static let ADD_ASSET = BASE_URL+"v1.0/driver/add/asset"
+    static let BOOKINGS = BASE_URL+"v1.0/driver/bookings"
+    
+}
